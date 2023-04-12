b3e6773ed210ab2684250366e9bdab74efd5dc1d
Alex E. Lea <alea01@vm-hw09.eecs.tufts.edu>
2023-04-12 16:08:29 -0400

    Cursor stuff

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,41 @@
-if __name__ == "__main__":
-    board = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+from time import sleep
 
+insert_mode = 0
+insert_index = 0
+
+def print_board(board, cursor = [-1,-1]):
+    sleep(0.5)
     output = ""
     for i in range(0, 9):
         for j in range(0, 9):
             if not j % 3:
                 output = output + "| "
-            output = output + F"{board[i][j]} "
+            if [i,j] == cursor:
+                output = output + "_ "
+            else:
+                output = output + F"{board[i][j]} "
 
         if (not i % 3):
             print(" ")
         print(output + "|")
         output = ""
+    sleep(0.5)
+    for i in range(0, 9):
+        for j in range(0, 9):
+            if not j % 3:
+                output = output + "| "
+            output = output + F"{board[i][j]} "
+        if (not i % 3):
+            print(" ")
+        print(output + "|")
+        output = ""
+
+
+if __name__ == "__main__":
+    board = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+    cursor = [0, 0]
+    inserts = "_123456789"
+    while (1):
+        print_board(board, cursor)
