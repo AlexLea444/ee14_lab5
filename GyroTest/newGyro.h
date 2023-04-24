9443cb861686615c451856316b651adc5c3a3906
Remy Ren <rren02@vm-hw01.eecs.tufts.edu>
2023-04-23 21:48:19 -0400

    GyroTest: Initialization Complete!

---
+++ GyroTest/newGyro.h
@@ -0,0 +1,10 @@
+#ifndef __STM32L476G_DISCOVERY_NEWGYRO_H
+#define __STM32L476G_DISCOVERY_NEWGYRO_H
+
+#include "stm32l476xx.h"
+
+void NewGyroInit();
+void displayXYZ();
+void printTest(char* str);
+
+#endif /* __STM32L476G_DISCOVERY_GYRO_H */
